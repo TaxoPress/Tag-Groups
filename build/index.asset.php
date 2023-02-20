105efd15f7dcafa26cef3fff8e332c8ad1d1a62e
Ojo Paul <ojopaul.dev@gmail.com>
2023-02-20 09:55:59 +0100

    - Remove the icons in Gutenberg blocks #15

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-url'), 'version' => '637bf2615ae76d3a7c3f2c72fe88ec45');
+<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-url'), 'version' => '15ecdccd9076d1143fd95971b9c6051d');
