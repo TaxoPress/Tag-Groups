4856c1cb818953ac312a0448129f5bff6b560655
deji98 <adesinafelix98@gmail.com>
2024-12-05 10:05:46 -0800

    remove the migrate feature in tag groups

--- views/admin/settings_troubleshooting_first_aid.view.php
+++ views/admin/settings_troubleshooting_first_aid.view.php
@@ -1,12 +1,6 @@
 <div class="tg_settings_tabs_content">
 
   <p>&nbsp;</p>
-  <form method="POST" action="<?php echo esc_url( add_query_arg( array( 'process-tasks' => $tasks_migration, 'task-set-name' => 'Migration' ) ) ) ?>">
-    <p><?php _e( 'Migrate tags to the new format of Tag Groups.', 'tag-groups' ) ?><span class="dashicons dashicons-editor-help chatty-mango-help-icon" title="<?php _e( 'The plugin automatically migrates tags to the new format and tries to keep track of changes. There may, however, be cases where other components made unnoticed changes. You might also need to manually migrate tags after you enabled a taxonomy that already contained untracked tags.', 'tag-groups' ) ?>"></span></p>
-    <input class="button-primary" type="submit" name="update" value="<?php _e( 'Migrate', 'tag-groups' ) ?>" id="submitbutton" />
-  </form>
-
-  <p>&nbsp;</p>
   <form method="POST" action="<?php echo esc_url( add_query_arg( array( 'process-tasks' => $tasks_maintenance, 'task-set-name' => 'Maintenance' ) ) ) ?>">
     <p><?php _e( 'Running the maintenance can solve issues that you encounter during filtering by tag groups.', 'tag-groups' ) ?></p>
     <input class="button-primary" type="submit" name="maintenance" value="<?php _e( 'Maintenance', 'tag-groups' ) ?>" id="submitbutton" />
