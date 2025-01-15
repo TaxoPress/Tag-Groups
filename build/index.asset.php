be3ea266c2489bba170887910eb7621b89249861
deji98 <adesinafelix@gmail.com>
2025-01-15 05:09:36 -0800

    composer updated

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => '141156b7672f1c1eea38');
+<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => 'f9b66cc55faf094460dc');
