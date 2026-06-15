1a27c10e927ec603912811c77b44e84c4ad598e6
Adedeji Adesina <adesinafelix98@gmail.com>
2026-06-15 21:38:47 +0100

    properly place ignore comments

--- views/partials/admin_footer_rating.view.php
+++ views/partials/admin_footer_rating.view.php
@@ -1,7 +1,7 @@
 <p class="footer_st">
     <?php
-    // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
     /* translators: %1$s and %2$s are opening and closing anchor tags, %3$s is the version number */
+        // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
     printf(__('Thanks for using Tag Groups | %1$sTaxoPress.com%2$s | Version %3$s', 'tag-groups'), '<a target="_blank" href="https://taxopress.com/">', '</a>', esc_html(TAG_GROUPS_VERSION));
     ?>
 </p>
