d44d4454973949678f02bf9e7705536407fd69fd
Adedeji Adesina <adesinafelix98@gmail.com>
2026-06-17 16:03:24 +0100

    update wp-cli.yml to target make-pot add translator comments where needed regenerate pot file

--- views/partials/admin_footer_rating.view.php
+++ views/partials/admin_footer_rating.view.php
@@ -1,7 +1,6 @@
 <p class="footer_st">
     <?php
-    /* translators: %1$s and %2$s are opening and closing anchor tags, %3$s is the version number */
-        // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
-    printf(__('Thanks for using Tag Groups | %1$sTaxoPress.com%2$s | Version %3$s', 'tag-groups'), '<a target="_blank" href="https://taxopress.com/">', '</a>', esc_html(TAG_GROUPS_VERSION));
+    /* translators: 1: Opening TaxoPress link tag, 2: Closing link tag, 3: Plugin version. */
+    printf(__('Thanks for using Tag Groups | %1$sTaxoPress.com%2$s | Version %3$s', 'tag-groups'), '<a target="_blank" href="https://taxopress.com/">', '</a>', esc_html(TAG_GROUPS_VERSION)); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
     ?>
 </p>
